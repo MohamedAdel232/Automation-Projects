c34d438a29735419248c145586867dce5b1fb58a
Mohamed <mohmdadl232@gmail.com>
2024-11-15 19:49:59 +0200

    Test Case 2: Login User with correct email and password

--- SeleniumProject/src/main/java/Pages/P02_LoginPage.java
+++ SeleniumProject/src/main/java/Pages/P02_LoginPage.java
@@ -9,6 +9,10 @@ public class P02_LoginPage {
     private final By nameFieldLocator = By.xpath("//input [@type=\"text\"]");
     private final By emailFieldLocator = By.xpath("//input [@data-qa=\"signup-email\"]");
     private final By signupButtonLocator = By.xpath("//button [@data-qa=\"signup-button\"]");
+    private final By loginToYourAccountTextLocator = By.xpath("//h2 [1]");
+    private final By loginEmailFieldLocator = By.xpath("//input [@data-qa=\"login-email\"]");
+    private final By loginPasswordFieldLocator = By.xpath("//input [@data-qa=\"login-password\"]");
+    private final By loginButtonLocator = By.xpath("//button [@data-qa=\"login-button\"]");
 
     private final WebDriver driver;
 
@@ -34,5 +38,24 @@ public class P02_LoginPage {
     public P03_SignupPage clickOnSignupButton() {
         Utility.clickOnElement(driver, signupButtonLocator);
         return new P03_SignupPage(driver);
+    }
+
+    public boolean verifyVisibilityOfLoginToYourAccountText() {
+        return Utility.checkVisibilityOfElement(driver, loginToYourAccountTextLocator);
+    }
+
+    public P02_LoginPage enterLoginEmail(String email) {
+        Utility.sendDataToElement(driver, loginEmailFieldLocator, email);
+        return this;
+    }
+
+    public P02_LoginPage enterLoginPassword(String password) {
+        Utility.sendDataToElement(driver, loginPasswordFieldLocator, password);
+        return this;
+    }
+
+    public P01_HomePage clickOnLoginButton() {
+        Utility.clickOnElement(driver, loginButtonLocator);
+        return new P01_HomePage(driver);
     }
 }
