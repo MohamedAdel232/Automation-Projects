14771c9059b108c55c32fc0b4503306c337774f9
Mohamed <mohmdadl232@gmail.com>
2024-11-18 11:17:01 +0200

    Test Case 24: Download Invoice after purchase order

--- SeleniumProject/src/main/java/Pages/P15_OrderPlacedPage.java
+++ SeleniumProject/src/main/java/Pages/P15_OrderPlacedPage.java
@@ -4,9 +4,13 @@ import Utilities.Utility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class P15_OrderPlacedPage {
     private final By orderPlacedTextLocator = By.xpath("//b [.=\"Order Placed!\"]");
     private final By deleteAccountButtonLocator = By.xpath("//a [@href=\"/delete_account\"]");
+    private final By downloadInvoiceButtonLocator = By.xpath("//a [.=\"Download Invoice\"]");
 
     private final WebDriver driver;
 
@@ -21,5 +25,13 @@ public class P15_OrderPlacedPage {
     public P05_DeleteAccountPage clickOnDeleteAccountButton() {
         Utility.clickOnElement(driver, deleteAccountButtonLocator);
         return new P05_DeleteAccountPage(driver);
+    }
+
+    public P15_OrderPlacedPage clickOnDownloadInvoiceButton() throws AWTException {
+        Utility.clickOnElement(driver, downloadInvoiceButtonLocator);
+        Robot robot = new Robot();
+        robot.delay(1000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        return this;
     }
 }
