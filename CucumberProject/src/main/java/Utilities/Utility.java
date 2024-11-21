d191951578d1d3f1293c2b836d5076921ceaa993
Mohamed <mohmdadl232@gmail.com>
2024-11-21 21:34:20 +0200

    Project Construction

---
+++ CucumberProject/src/main/java/Utilities/Utility.java
@@ -0,0 +1,48 @@
+package Utilities;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+public class Utility {
+    public static final String screenshotPath = "test-outputs/Screenshots/";
+
+    public static void clickOnElement(WebDriver driver, By locator) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+        driver.findElement(locator).click();
+    }
+
+    public static void sendDataToElement(WebDriver driver, By locator, String data) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+        driver.findElement(locator).sendKeys(data);
+    }
+
+    public static String getDataFromElement(WebDriver driver, By locator) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return driver.findElement(locator).getText();
+    }
+
+    public static String getTimestamp() {
+        return new SimpleDateFormat("yyyy-MM-d-h-m-sa").format(new Date());
+    }
+
+    public static void takeScreenshot(WebDriver driver, String imageName) throws IOException {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File src = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File dest = new File(screenshotPath + imageName + "-" + getTimestamp() + ".png");
+        FileUtils.copyFile(src, dest);
+    }
+}
