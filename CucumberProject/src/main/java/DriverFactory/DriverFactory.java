e2551cc94f20f8db2229b1258557c150e0dfdf23
Mohamed <mohmdadl232@gmail.com>
2024-11-22 13:08:12 +0200

    Project Construction

---
+++ CucumberProject/src/main/java/DriverFactory/DriverFactory.java
@@ -0,0 +1,48 @@
+package DriverFactory;
+
+import Utilities.LogsUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class DriverFactory {
+    public static ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
+
+    public static void setupDriver(String browser) {
+        switch (browser) {
+            case "Firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--start-maximized");
+                firefoxOptions.addArguments("--guest");
+                driverThreadLocal.set(new FirefoxDriver());
+                LogsUtils.info("Browser: " + browser + "is opened");
+                break;
+            case "Edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                edgeOptions.addArguments("--guest");
+                driverThreadLocal.set(new EdgeDriver(edgeOptions));
+                LogsUtils.info("Browser: " + browser + "is opened");
+                break;
+            default:
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--guest");
+                driverThreadLocal.set(new ChromeDriver(chromeOptions));
+                LogsUtils.info("Browser: " + browser + "is opened");
+                break;
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driverThreadLocal.get();
+    }
+
+    public static void quitDriver() {
+        getDriver().quit();
+    }
+}
