86dd6400ba74d5472f2e1d867d26a13f11a393e9
Mohamed <mohmdadl232@gmail.com>
2024-12-02 14:50:09 +0200

    Modified The Typing Project

--- TypingProject/src/test/java/TestCase.java
+++ TypingProject/src/test/java/TestCase.java
@@ -1,10 +1,8 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -14,7 +12,7 @@ import java.util.List;
 
 public class TestCase {
     @Test
-    public void testCase(){
+    public void testCase() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         WebDriver driver = new ChromeDriver(options);
@@ -25,13 +23,13 @@ public class TestCase {
         driver.findElement(By.xpath("//button [@id= \"CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll\"]"))
                 .click();
 
-        List list = driver.findElements(By.xpath("//span [@wordnr]"));
-        for (int i = 1; i <= list.size(); i++){
+        List<WebElement> list = driver.findElements(By.xpath("//span [@wordnr]"));
+        for (int i = 1; i <= list.size(); i++) {
             String text;
-            By tempLocator = By.xpath("(//span [@wordnr])["+i+"]");
+            By tempLocator = By.xpath("(//span [@wordnr])[" + i + "]");
             text = driver.findElement(tempLocator).getText();
             driver.findElement(By.xpath("//input [@type=\"text\"]"))
-                    .sendKeys(text  + " ");
+                    .sendKeys(text + " ");
         }
     }
 }
